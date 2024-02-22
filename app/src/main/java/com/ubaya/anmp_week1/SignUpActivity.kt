96f310bbf13707bb00c404afd53fb56f6a08cf17
TrueGre3n <oeyraymon4@gmail.com>
2024-02-22 14:28:09 +0700

    signup comend

--- app/src/main/java/com/ubaya/anmp_week1/SignUpActivity.kt
+++ app/src/main/java/com/ubaya/anmp_week1/SignUpActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //signup for user
+        //1.check existing account
+        //2.check password
+        //3.implement
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
